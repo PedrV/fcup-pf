5188a9d0e4411cf5c06644bb3fc7a8417e169a0c
Pedro Vieira <46421895+PedrV@users.noreply.github.com>
2020-02-17 20:29:38 +0000

    new exercices solutions

---
+++ Exercices/exercices1_2.hs
@@ -0,0 +1,77 @@
+-- 1
+incr :: Num a => a -> a
+incr x = x + 1
+
+quadrado :: Num a => a -> a
+quadrado x = x*x
+
+dobro :: Num a => a -> a
+dobro a = a+a
+
+media :: Float -> Float -> Float
+media x y = (x+y)/2
+
+{-
+a) 26
+b) 36
+c) 6.0
+-}
+
+-- 2
+
+triangulo :: Int -> Int -> Int -> Bool
+triangulo x y z = 
+    if x < y + z
+        then True
+        else if y < x + z
+            then True
+            else if z < y + x
+                then True
+                else False
+
+-- Guardas
+neatTriangulo :: Int -> Int -> Int -> Bool
+neatTriangulo x y z
+        | x < y + z = True
+        | y < x + z = True
+        | z < y + x = True 
+        | otherwise = False
+
+
+-- 3
+
+area :: Float -> Float -> Float -> Float
+area x y z = 
+    sqrt (s*(s-x)*(s-y)*(s-z))
+    where s = (x + y + z)/2
+
+-- 4
+
+metade :: [a] -> ([a],[a])
+metade xs = (take len xs, drop len xs)
+            where len = div (fromIntegral(length xs)) 2 
+
+-- 5
+--a)
+
+last1 :: [a] -> a
+last1 [] = error "empty list"
+last1 xs = xs !! l
+        where l = (-) (length xs) 1 -- get the last index
+
+last2 :: [a] -> a
+last2 [] = error "empty list"
+last2 [x] = x
+last2 (_:xs) = last2 xs -- tornar a lista cada vez mais pequena ate sobrar so 1 que é o ultimo
+
+-- b)
+
+init1 :: [a] -> [a]
+init1 [] = error "empty list"
+init1 xs = take l xs
+        where l = (-) (length xs) 1 -- get the last index
+
+init2 :: [a] -> [a]
+init2 [] = error "empty list"
+init2 [x] = []
+init2 (x:xs) = x : init2 xs 
