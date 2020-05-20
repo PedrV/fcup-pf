05c7fc3152e7a9e925e55c6912d71447b9e817e9
PedrV <46421895+PedrV@users.noreplay.github.com>
2020-05-20 13:06:43 +0100

    Week13: test corr

--- Exercices/Week12/testeIntercalar.hs
+++ Exercices/Week12/testeIntercalar.hs
@@ -7,19 +7,20 @@
 --d) [16..32]
 --e) [(3,2), (4,3), (5,4), (5,6), (6,8), (7,12)]
 --f) [[2,8],[4,6],[]]
+--g) [x <- zip [0..6] [6,5..0], x + y == 6]
 --h) 15
 --i) [(Char, String)]
---j) f :: (Num a) -> a -> [a] -> Bool
+--j) f :: (Ord a, Num a) => a -> [a] -> Bool
 --k) ig :: Eq a => [a] -> Bool
 --l) fix :: Eq a => (a -> a) -> a -> Bool
 
 --2 
 --a)
+-- Afinal nem sequer é preciso verificar que é triangulo, e é garantido que ints sao positivos
 pitagoricos :: (Ord a, Num a) => a -> a -> a -> Bool
 pitagoricos a b c | a >= b && a >= c      = (a*a) == (b*b) + (c*c)
                   | b >= a && b >= c      = (b*b) == (c*c) + (a*a)
                   | c >= a && c >= b      = (c*c) == (b*b) + (a*a)
-                  | otherwise             = error "pitagoricos: Not a triangle (rectangle)"
 
 --b)
 hipotenusa :: Float -> Float -> Float
@@ -30,13 +31,14 @@ hipotenusa a b = sqrt((a*a) + (b*b))
 --a)
 diferentes :: Eq a => [a] -> [a]
 diferentes [] = []
-diferentes [x] = [x]
+diferentes [x] = []
+-- diferentes [x] = [x] ultimo elem é sempre eliminado
 diferentes (x:y:xs) | x /= y    = x : diferentes (y:xs)
                     | otherwise = diferentes (y:xs)
 
 --b)
 diferentes' :: Eq a => [a] -> [a]
-diferentes' xs = [ fst y | y <- zip xs (tail xs), (fst y) /= (snd y)] ++ [last xs]
+diferentes' xs = [ fst y | y <- zip xs (tail xs), (fst y) /= (snd y)] -- ++ [last xs]  ultimo ele é eliminado
 
 
 --4
@@ -46,8 +48,8 @@ zip3' xs ys zs = [(fst (fst l), snd (fst l), snd l) | l<- zip (zip xs ys) zs]
 --5
 partir :: Eq a => [a] -> a -> ([a],[a])
 partir [] _ = ([],[])
-partir (x:xs) n | x == n    = ([],[])
-                | otherwise = ((x : fst (partir xs n)), (dropWhile (/= n) xs))
+partir (x:xs) n | x == n    = ([], x:xs)
+                | otherwise = ((x : fst (partir xs n)), snd (partir xs n))
 
 -- Incompleto
 --6 
