b8f10781b988f4848bcc066884ec16ff2eccdec3
PedrV <46421895+PedrV@users.noreplay.github.com>
2020-06-08 23:08:25 +0100

    Prep Ex: day 1

---
+++ Exercices/Remake/ex1.hs
@@ -0,0 +1,74 @@
+inc x = x+1 
+
+dobro x = x + x 
+
+quadrado x = x * x
+
+media x y = (x+y)/2
+
+--2
+triangulo :: Int -> Int -> Int -> Bool
+triangulo x y z | x >= y+z  = False
+                | y >= z+x  = False
+                | z >= x+y  = False
+                | otherwise = True
+--3
+area :: Float -> Float -> Float -> Float
+area x y z = sqrt(s*(s-x)*(s-y)*(s-z))
+            where s = z+x+y
+
+--4
+split :: [a] -> ([a],[a])
+split xs = (take l xs, drop l xs)
+        where l = length xs `div` 2
+
+--5
+last' :: [a] -> a
+last' xs = head $ drop ((length xs) - 1) xs
+
+init' :: [a] -> a
+init' (x:xs) = x
+
+--6
+bino :: Float -> Float -> Float
+bino n k = product [1..n] / (product ([1..k] ++ [1..(n-k)]))
+
+--7
+max3 :: Ord a => a -> a -> a -> a
+max3 a b c  | a >= c && a >= b   = a
+            | c >= a && c >= a   = c
+            | otherwise          = b
+
+max3' :: Ord a => a -> a -> a -> a
+max3' a b c = max c (max a b)
+
+--8
+orderTriple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+orderTriple (a,b,c) = (x,y,z)
+                    where x = min a (min b c)
+                          y = min a (max b c)
+                          z = max a (max b c)
+
+--9 
+classifica :: Int -> String
+classifica x | x <= 9        = "Reprovado"
+             | x <= 12       = "Suficiente"
+             | x <= 15       = "Bom"
+             | x <= 18       = "Muito Bom"
+             | otherwise     = "Muito Bom Com Distincao"
+
+--10
+xor :: Bool -> Bool -> Bool
+xor p1 p2 | p1 /= p2    = True
+          | otherwise   = False
+
+--11
+safetail :: [a] -> [a]
+safetail [] = []
+safetail (x:xs) = xs
+
+--12
+curta :: [a] -> Bool
+curta xs | length xs <= 2   = True
+         | otherwise        = False
+
---
+++ Exercices/Remake/ex2.hs
@@ -0,0 +1,71 @@
+{- 14 
+a) [Char] 
+b) (Char,Char,Char) 
+c) [(Bool,Char)]
+d) ([Bool], [Char])
+e) [([a] -> [a])]
+f) [(Bool -> Bool)]
+ -}
+
+{- 15 
+1) f :: (Int,Int) -> Int
+2) f :: Int -> Int, g :: Int -> Int
+3) f :: (Int -> Int) -> Int -> Int, g :: Int -> Int
+4) f :: Int -> [Int], g :: [Int] -> Int
+5) f :: (Int,Int) -> [Int -> Int]
+ -}
+
+ --16
+ -- f :: (a -> a) -> (Int -> [[Int]])
+ -- g :: a -> a
+
+{- 17 
+a) segundo :: [a] -> a
+b) trocar :: (a,b) -> (b,a)
+par :: a -> b -> (a,b)
+dobro :: Num a => a -> a
+metade :: Fractional a => a -> a
+minuscula :: Char -> Bool
+intervalo :: Ord a => a -> a -> a -> Bool
+paliandro :: [a] -> Bool
+twice :: (a -> a) -> a -> a 
+ -}
+
+--19
+--1 
+f :: Int -> (Int -> Int) -> Int
+f x g = g x
+
+--2
+f1 :: Char -> Bool -> Bool
+f1 a p = a == 'p' && p
+
+--3
+f2 :: (Char -> Char -> Int) -> Char -> Int
+f2 g1 a = g1 a a
+
+--4
+f3 :: Eq a => a -> [a] -> Bool
+f3 y xs = y == (head xs)
+
+--5
+f4 :: Eq a => [a] -> [a] -> Bool
+f4 xs ys = xs == ys
+
+--6
+f5 :: Ord a => a -> a -> a 
+f5 x y | x > y     = y
+       | otherwise = x
+
+--20
+-- f :: (a,[a]) -> Bool
+-- (2,[3]) Sim, Bool 
+-- (2,[]) Sim, Bool
+-- (2,[True]) Não, erro de tipos entre int e bool
+
+--21
+-- f:: (a,[a]) -> a
+-- (2,[3]) Sim, Int 
+-- (2,[]) Sim, Int
+-- (2,[True]) Não, erro de tipos entre int e bool
+
