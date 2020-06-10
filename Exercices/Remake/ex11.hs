946571248ed581a500ba9729bd7c0888b5f397f9
PedrV <46421895+PedrV@users.noreplay.github.com>
2020-06-11 00:05:24 +0100

    Exam: 3rd day

--- Exercices/Remake/ex11.hs
+++ Exercices/Remake/ex11.hs
@@ -1,11 +1,178 @@
+import Stack
+
 --78
-data Shape = Rectancgle Float Float) | (Circle Float)
+--a)
+data Shape = Rectangle Float Float | Circle Float deriving (Eq,Ord)
 
+instance Show Shape where
+    show (Rectangle f1 f2) = "Rectangle" ++ show f1 ++ show f2
+    show (Circle r) = "Circle" ++ show r
 
+--b)
+perimetro :: Shape -> Float
+perimetro (Circle r) = 3.14 * (2*r)
+perimetro (Rectangle f1 f2) = 2 * f1 + 2 * f2
 
 --79
+data Retangulo = R (Int,Int) (Int, Int) deriving (Eq,Ord,Show)
+
+area :: Retangulo -> Int
+area (R (x1,y1) (x2,y2)) = (x2-x1) * (y2-y1)
+
+inter :: Retangulo -> Retangulo -> Bool
+inter (R (x1,y1) (x2,y2)) (R (x3,y3) (x4,y4)) | x3 > x2 || x4 < x1   = False
+                                              | y3 > y2 || y4 < y1   = False
+                                              | otherwise            = True  
+
 --80
+
+parent' :: String -> Bool
+parent' xs = parent xs empty
+
+
+parent :: String -> Stack Char -> Bool
+parent "" s = isEmpty s
+parent (x:xs) s | x == '('        = parent xs (push x s)
+                | x == '['        = parent xs (push x s)
+                | x == ')'        = if isEmpty s then 
+                                    False else 
+                                        if top s == '(' then 
+                                            parent xs (pop s) else False
+                | x == ']'        = if isEmpty s then
+                                    False else
+                                        if top s == '[' then 
+                                            parent xs (pop s) else False
+                | otherwise       = parent xs s
+
+
+
 --81
+--a)
+
+size1 :: Stack a -> Int
+size1 s | isEmpty s     = 0
+        | otherwise     = 1 + size1 (pop s)
+
+calc :: Stack Float -> [String] -> Stack Float
+calc s [] = s
+calc s (x:xs) | x == "*"        = if size1 s < 2 then error "calc: expressao incorreta" 
+                                  else calc (push (top s * top (pop s)) (pop $ pop s)) xs
+              | x == "/"        = if size1 s < 2 then error "calc: expressao incorreta" 
+                                  else calc (push (top s / top (pop s)) (pop $ pop s)) xs
+              | x == "-"        = if size1 s < 2 then error "calc: expressao incorreta" 
+                          4        else calc (push (top s - top (pop s)) (pop $ pop s)) xs
+              | x == "+"        = if size1 s < 2 then error "calc: expressao incorreta" 
+                                  else calc (push (top s + top (pop s)) (pop $ pop s)) xs
+              | otherwise       = calc (push (read x) s) xs
+
+--b)
+calcular :: String -> Float
+calcular xs = top $ calc empty (words xs)
+
+
+--c)
+rpn :: IO ()
+rpn = do
+    string <- getLine
+    if string == "Done" then
+        return ()
+        else do
+            putStrLn (show $ calcular string)
+            rpn
+
 --82
+data Graph = Graph ( [Int] , [(Int, Int)] )
+
+grafo :: Graph
+grafo = Graph ([1..6], [(1,2), (1,4), (2,3), (2,5), (2,6), (3,6), (4,5), (6,5)])
+
+destinos :: Graph -> Int -> [Int]
+destinos (Graph (v, a)) o = [snd x | x<- a, fst x == o]
+
+caminhos :: Graph -> Int -> Int -> Int
+caminhos (Graph (v,a)) o d | (o == d)   = 1
+                           | otherwise  = sum [ caminhos (Graph (v,a)) x d | x <- dest  ]
+                           where dest = destinos (Graph (v,a)) o
+
+
+
 --84
+{- data Map = Map Char Int
+
+instance Eq Map where
+    (==) (Map c i) (Map c1 i1) = c == c1
+
+instance Show Map where
+    show (Map c i) = show c ++ ":" ++ show i
+
+instance Num Map where
+    (+) (Map c i) (Map c1 i1) | c == c1   = Map c i+1
+                              | otherwise = Map c i
+    (-) (Map c i) (Map c1 i1) | c == c1   = Map c i-1
+                              | otherwise = Map c i
+    fromInteger 
+    
+
+contains :: [Map] -> Map -> Bool
+contains [] k = False
+contains (x:xs) k | k == x    = True
+                  | otherwise = contains xs k
+
+get :: [Map] -> Map -> Map
+get [] m = m
+get (x:xs) m | x == m       = x
+             | otherwise    = get xs m
+
+
+chars :: String -> [Map]
+chars xs = foldr (\a b -> ((get b (Map a 0)) + (Map a 1)) : b) [] xs -}
+
+
 --88
+data Map k v = No (k,v) (Map k v) (Map k v) | EmptyMap deriving (Show,Ord,Eq)
+
+data Tree a = Node a (Tree a) (Tree a) | Leaf deriving (Show, Eq, Ord)
+
+
+qsort :: Ord a => [(a,b)] -> [(a,b)]
+qsort [] = []
+qsort (x:xs) = qsort left ++ [x] ++ qsort right
+              where left = [ y | y<-xs, (fst y) < (fst x) ]
+                    right = [ y | y<-xs, (fst y) > (fst x) ]
+
+
+emptyMap :: Map k v
+emptyMap = EmptyMap
+
+insert :: Ord k => k -> v -> Map k v -> Map k v
+insert k a EmptyMap = No (k,a) EmptyMap EmptyMap
+insert k a (No (x,y) mleft mright) | k < x      = No (x,y) (insert k a mleft) mright
+                                   | k > x      = No (x,y) mleft (insert k a mright)
+                                   | otherwise  = No (x,y) mleft mright
+
+mais_dir :: Map k v -> (k,v)
+mais_dir (No (k,v) _ EmptyMap) = (k,v)
+mais_dir (No (k,v) _ mright) = mais_dir mright
+
+
+remove :: Ord k => k -> Map k v -> Map k v
+remove k (No (x,y) EmptyMap EmptyMap) = if k == x then EmptyMap else No (x,y) EmptyMap EmptyMap
+remove k (No (x,y) mleft mright) | k < x        = No (x,y) (remove k mleft) mright
+                                 | k > x        = No (x,y) mleft (remove k mright)
+                                 | otherwise    = No z mleft mright
+                                 where z = mais_dir mleft
+
+constructMap :: Ord k =>  [(k,v)] -> Map k v
+constructMap [] = EmptyMap
+constructMap xs = No m (constructMap left) (constructMap right)
+                where m = (!!) list l 
+                      left = take l list
+                      r:right = drop l list
+                      l = (length list) `div` 2
+                      list = qsort xs 
+
+lookup' :: Ord k => k -> Map k v -> Maybe k
+lookup' _ EmptyMap = Nothing
+lookup' k (No (x,y) mleft mright) | k < x        = lookup' k mleft
+                                  | k > x        = lookup' k mright
+                                  | otherwise    = Just x
---
+++ Exercices/Remake/ex12.hs
@@ -0,0 +1,337 @@
+{- 
+
+-- Propriedades
+
+-0 
+Todas as distribuiçoes e associaçoes da matemica estao presentes
+
+-1
+reverse [] = []                         (rev1)
+reverse (x:xs) = reverse xs ++ [x]      (rev2)
+
+-2
+[] ++ ys = ys                           (++1)    
+(x:xs) ++ ys = x:(xs++ys)               (++2)    
+
+-3
+Zero + m = m                            (+1)
+(Succ n) + m = Succ (n+m)               (+2)    
+
+
+-4
+length [] = 0                           (l1)
+length (x:xs) = 1 + length xs           (l2)
+
+
+-5
+replicate 0 x = []                      (rep1)
+replicate n x = x : replicate (n-1) x   (rep2)
+
+ -}
+
+
+--89
+
+Objetivo: x + (y+z) = (x+y) + z
+
+Caso Base:
+         
+        Zero + (x+z)   (Assoc +)
+        (Zero + y) + z  (+1)
+        y + z           (+1)
+        (Zero + y) + z
+
+Caso de Inducao:
+
+        x + (y+z) = (x+y) + z => (Succ x) + (y+z) = ((Succ x)+y) + z
+
+        Hipotese de Inducao:  x + (y+z) = (x+y) + z
+
+        (Succ x) + (y+z)   (+2) 
+        Succ (x + (y+z))    H.I
+        Succ ((x+y) + z)   (+2)
+        (Succ (x+y)) + z   (+2)       
+        ((Succ x) + y) + z
+
+---------------------------------------------------------------------------------------------------
+
+
+--90
+
+Objetivo: (xs ++ ys) ++ zs = xs ++ (ys ++ zs) (Assoc ++)
+
+Caso Base:
+        ([] ++ ys) ++ zs    (++1)
+        ys ++ zs            (++1)
+        [] ++ (ys ++ zs)
+
+Caso de Inducao:
+
+        (xs ++ ys) ++ zs = xs ++ (ys ++ zs) => ((x:xs) ++ ys) ++ zs = (x:xs) ++ (ys ++ zs)
+
+        Hipotese de Inducao: (xs ++ ys) ++ zs = xs ++ (ys ++ zs)
+
+        ((x:xs) ++ ys) ++ zs    (++2)
+        (x:(xs++ys)) ++ zs      (++2)
+        x:((xs++ys) ++ zs)      H.I
+        x:(xs ++ (ys ++ zs))    (++2)
+        (x:xs) ++ (ys ++ zs)
+
+---------------------------------------------------------------------------------------------------
+
+--91
+
+-- Lemma (++3) --
+
+Objetivo: ys ++ [] = ys
+
+        [] ++ [] (++1)
+        []
+
+Caso de Inducao: 
+     
+        ys ++ [] = ys => (y:ys) ++ [] = (y:ys)
+
+        Hipotese de Inducao: ys ++ [] = ys
+
+        (y:ys) ++ []   (++2)
+        y:(ys ++ [])   H.I 
+        (y:ys)
+
+--------------------------------
+
+Objetivo: reverse (xs++ys) = reverse ys ++ reverse xs
+
+Caso Base: 
+        reverse ([]++ys)        (++1)
+        reverse (ys)            (++3)
+        reverse ys ++ []        (rev1)
+        reverse ys ++ reverse []
+
+
+Caso de Inducao: 
+        reverse (xs++ys) = reverse ys ++ reverse xs => reverse ((x:xs)++ys) = reverse ys ++ reverse (x:xs)
+
+        Hipotese de Inducao: reverse (xs++ys) = reverse ys ++ reverse xs
+
+        reverse ((x:xs)++ys)                (++2)
+        reverse (x:(xs++ys))                (rev2)
+        reverse (xs++ys) ++ [x]             H.I
+        (reverse ys ++ reverse xs) ++ [x]   (Assoc ++)
+        reverse ys ++ (reverse xs ++ [xs])  (rev2)
+        reverse ys ++ reverse (x:xs)
+
+---------------------------------------------------------------------------------------------------
+
+
+--92
+
+map f [] = []                       (m1)
+map f (x:xs) = f x : map f xs       (m2)
+
+(f . g) x = f (g x)                 (comp)
+
+
+Objetivo: map f (map g xs) = map (f . g) xs
+
+Caso Base: 
+        map f (map g [])        (m1)
+        map f []                (m1)
+        []                      (m1)
+        map (f . g) []          
+
+Caso de Inducao: 
+        map f (map g xs) = map (f . g) xs => map f (map g (x:xs)) = map (f . g) (x:xs)
+
+
+Hipotese Inducao: map f (map g xs) = map (f . g) xs
+
+        map f (map g (x:xs))            (m2)
+        map f ((g x) : map g xs)        (m2)
+        (f (g x)) : (map f (map g xs))  H.I
+        (f (g x)) : map (f . g) xs     (comp)
+        (f . g) x : map (f . g) xs     (m2) 
+        map (f . g) (x:xs)
+
+---------------------------------------------------------------------------------------------------
+
+
+--93
+
+take 0 xs = []                              (t1)
+take n [] | n > 0 = []                      (t2)
+take n (x:xs) | n > 0 =  x: take (n-1) xs   (t3)
+
+drop 0 xs = xs                              (d1)
+drop n []| n > 0 = []                       (d2)
+drop n (x:xs) |n > 0 = drop (n−1) xs        (d3)    
+
+
+Objetivo: take n xs ++ drop n xs = xs
+
+
+Caso Base:  
+
+        take 0 xs ++ drop 0 xs    (t1) 
+        [] ++ drop 0 xs           (d1)
+        [] ++ xs                  (++1) 
+        xs
+
+Caso Inducao: 
+
+        take n xs ++ drop n xs = xs => take (n+1) xs ++ drop (n+1) xs = xs
+
+Hipotese de Inducao: take n xs ++ drop n xs = xs
+
+-- xs = (x:xs)
+take (n+1) (x:xs) ++ drop (n+1) (x:xs)          (t3)
+(x : (take n xs)) ++ drop (n+1) (x:xs)          (d3)
+(x : (take n xs)) ++ drop n xs                  (++2) 
+x : (take n xs ++ drop n xs)                    H.I
+x:xs
+
+-- xs = []
+take (n+1) [] ++ drop (n+1) []                  (t2)
+[] ++ drop (n+1) []                             (d2)
+[] ++ []                                        [++1]
+[]
+
+---------------------------------------------------------------------------------------------------
+
+--94
+
+Objetivo: length (map f xs) = length xs
+
+Caso base:
+
+        length (map f [])       (m1)
+        length []
+
+Caso Inducao: 
+
+        length (map f xs) = length xs => length (map f (x:xs)) = length (x:xs)
+
+Hipotese: length (map f xs) = length xs
+
+length (map f (x:xs))           (m2)
+length ((f x) : (map f xs))     (l2)
+1 + length (map f xs)           H.I
+1 + length xs                   (l2)
+length (x:xs)
+
+---------------------------------------------------------------------------------------------------
+
+--95
+
+sum [] = 0               (s1)
+sum (x:xs) = x + sum xs  (s2)
+
+
+Objetivo: sum (map (1+) xs) = length xs + sum xs
+
+Caso Base: 
+
+        sum (map (1+) [])       (m1)
+        sum []                  (s1)
+        0                       (Elm neutro)   
+        0 + 0                   (l1) (s1)
+        length [] + sum []
+
+Caso de Inducao: 
+
+        sum (map (1+) xs) = length xs + sum xs => sum (map (1+) (x:xs)) = length (x:xs) + sum (x:xs)
+
+Hipotese de Inducao: sum (map (1+) xs) = length xs + sum xs 
+
+        sum (map (1+) (x:xs))               (m2)
+        sum ((1+x) : (map (1+) xs))         (s2)
+        (1+x) + sum (map (1+) xs)           H.I
+        (1+x) + (length xs + sum xs)        (Rearr)
+        1 + length xs + x + sum xs          (l2) (s2)
+        length (x:xs) + sum (x:xs)
+
+---------------------------------------------------------------------------------------------------
+
+--96
+
+Objetivo: map f (xs++ys) = map f xs ++ map f ys     (96)
+
+Caso Base: 
+        map f ([]++ys)              (++1)
+        map f ys                    (++1)
+        [] ++ map f ys              (m1)
+        map f [] ++ map f ys
+
+Caso de Inducao: 
+        map f (xs++ys) = map f xs ++ map f ys => map f ((x:xs)++ys) = map f (x:xs) ++ map f ys
+
+Hipotese de Inducao: map f (xs++ys) = map f xs ++ map f ys
+
+        map f ((x:xs)++ys)              (++2)
+        map f x:(xs++ys)                (m2)
+        (f x) : map f (xs++ys)          H.I
+        (f x) : (map f xs ++ map f ys)  (++2)
+        (f x : map f xs) ++ map f ys    (m2)
+        map f (x:xs) ++ map f ys
+
+---------------------------------------------------------------------------------------------------
+
+--97
+
+Objetivo: map f (reverse xs) = reverse (map f xs)
+
+Caso base:
+        map f (reverse [])      (rev1)
+        map f []                (m1)
+        []                      (rev1)
+        reverse []              (m1)      
+        reverse (map f [])
+
+Caso Inducao: 
+        map f (reverse xs) = reverse (map f xs) => map f (reverse (x:xs)) = reverse (map f (x:xs))
+
+Hipotese de Inducao: map f (reverse xs) = reverse (map f xs)
+
+        map f (reverse (x:xs))                  (rev1)
+        map f (reverse xs ++ [x])               (96)
+        map f (reverse xs) ++ map f [x]         H.I
+        reverse (map f xs) ++ map f [x]         (m2)
+        reverse (map f xs) ++ (f x) : map f []  (m1)
+        reverse (map f xs) ++ (f x) : []        (:1)
+        reverse (map f xs) ++ [f x]             (rev2)
+        reverse ((f x) : map f xs)              (m2)
+        reverse (map f (x:xs))
+
+---------------------------------------------------------------------------------------------------
+
+--98
+
+inserir x [] = [x]                                      (i1)
+inserir x (y:ys) | x <= y = x:y:ys                      (i2)
+inserir x (y:ys) | x > y  = y : inserir x ys            (i3)   
+
+Objetivo: length (insert x xs) = 1 + length xs
+
+Caso base: 
+        length (insert x [])        (i1)
+        length [x]                  (l2)               
+        1 + length []
+
+Caso de Inducao:
+        length (insert x xs) = 1 + length xs => length (insert x (x:xs)) = 1 + length (x:xs)
+
+Hipotese de Inducao: length (insert x xs) = 1 + length xs
+
+    -- z <= x
+        length (insert z (x:xs))        (i2)
+        length (z:x:xs)                 (l2)            
+        1 + length (x:xs)
+
+    -- z > x
+        length (insert z (x:xs))       (i3) 
+        length (x : insert z xs)       (l2) 
+        1 + length (insert z xs)       H.I
+        1 + 1 + length xs              (l2) 
+        1 + length (x:xs)
+
+---------------------------------------------------------------------------------------------------
